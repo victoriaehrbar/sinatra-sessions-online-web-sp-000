188dca6e976856ae1e061812e04a9b156e6b84e8
victoriaehrbar <victoriaehrbar@gmail.com>
2021-03-15 18:42:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ class App < Sinatra::Base
   end
 
      get '/fetch_session_id' do
-    "You did it! session[:user_id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
