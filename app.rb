6bac0196da2ebefc1ec24152f68561a94930e116
victoriaehrbar <victoriaehrbar@gmail.com>
2021-03-15 18:34:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -40,7 +40,6 @@ class App < Sinatra::Base
     session[:id] = 1
     #set session id here
     if session[:id] == 1
-      # "Session ID set. It's currently set to #{session[:user_id]}."
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
